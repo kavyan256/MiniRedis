567df80d92492efd4e42a266a423d995547ab241
kavyan2 <IIT2024245@iiita.ac.in>
2025-12-08 02:54:21 +0530

    added EXISTS, INCR/DECR, MGET/MSET, FLUSHALL

--- main.go
+++ main.go
@@ -169,6 +169,164 @@ func handleConnection(conn net.Conn) {
 					} else {
 						conn.Write([]byte("-ERR wrong number of arguments for 'PING' command\r\n"))
 					}
+
+				case "ECHO":
+					if len(args) != 2 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'ECHO' command\r\n"))
+						continue
+					}
+					message := args[1]
+					conn.Write([]byte("$" + strconv.Itoa(len(message)) + "\r\n" + message + "\r\n"))
+
+				case "EXISTS":
+					if len(args) != 2 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'EXISTS' command\r\n"))
+						continue
+					}
+					//example "EXISTS A"
+					count := 0
+					for _, key := range args[1:] {
+						mu.RLock()
+						_, exists := store[key]
+						mu.RUnlock()
+						if exists {
+							count++
+						}
+					}
+					conn.Write([]byte(":" + strconv.Itoa(count) + "\r\n"))
+
+				case "INCR":
+					if len(args) != 2 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'INCR' command\r\n"))
+						continue
+					}
+					//example "INCR A"
+					key := args[1]
+
+					mu.Lock()
+					value, exists := store[key]
+					if !exists {
+						store[key] = "1"
+						mu.Unlock()
+						conn.Write([]byte(":1\r\n"))
+						continue
+					}
+					
+					intValue, err := strconv.Atoi(value)
+					if err != nil {
+						mu.Unlock()
+						conn.Write([]byte("-ERR value is not an integer\r\n"))
+						continue
+					}
+					
+					intValue++
+					store[key] = strconv.Itoa(intValue)
+					mu.Unlock()
+					conn.Write([]byte(":" + strconv.Itoa(intValue) + "\r\n"))
+
+				case "DECR":
+					if len(args) != 2 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'DECR' command\r\n"))
+						continue
+					}
+					//example "DECR A"
+					key := args[1]
+
+					mu.Lock()
+					value, exists := store[key]
+					if !exists {
+						store[key] = "-1"
+						mu.Unlock()
+						conn.Write([]byte(":-1\r\n"))
+						continue
+					}
+					
+					intValue, err := strconv.Atoi(value)
+					if err != nil {
+						mu.Unlock()
+						conn.Write([]byte("-ERR value is not an integer\r\n"))
+						continue
+					}
+					
+					intValue--
+					store[key] = strconv.Itoa(intValue)
+					mu.Unlock()
+					conn.Write([]byte(":" + strconv.Itoa(intValue) + "\r\n"))
+
+				case "MGET":
+					if len(args) < 2 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'MGET' command\r\n"))
+						continue
+					}
+
+					numKeys := len(args) - 1
+					conn.Write([]byte("*" + strconv.Itoa(numKeys) + "\r\n"))
+
+					for _, key := range args[1:] {
+						mu.RLock()
+						value, exists := store[key]
+						mu.RUnlock()
+						if exists {
+							conn.Write([]byte("$" + strconv.Itoa(len(value)) + "\r\n" + value + "\r\n"))
+						} else {
+							conn.Write([]byte("$(NIL)\r\n"))
+						}
+					}
+
+				case "MSET":
+					if len(args) < 3 || len(args[1:])%2 != 0 {
+						conn.Write([]byte("-ERR wrong number of arguments for 'MSET' command\r\n"))
+						continue
+					}
+
+					mu.Lock()
+					for i := 1; i < len(args); i += 2 {
+						key := args[i]
+						value := args[i+1]
+						store[key] = value
+					}
+					mu.Unlock()
+					conn.Write([]byte("+OK\r\n"))
+
+				case "FLUSHALL":
+					if !( len(args) == 1 || (len(args) == 2) ){
+						conn.Write([]byte("-ERR wrong number of arguments for 'FLUSHALL' command\r\n"))
+						continue
+					}
+					
+					mode := "SYNC"
+					if len(args) == 2 {
+						mode = strings.ToUpper(args[1])
+					}
+
+					switch mode {
+						case "SYNC":
+							mu.Lock()
+							old := store
+							store = make(map[string]string)
+							mu.Unlock()
+							
+							go func(m map[string]string) {
+								_ = m //simulating background deletion
+							}(old)
+							
+							conn.Write([]byte("+OK\r\n"))
+						
+						case "ASYNC":
+							go func() {
+								mu.Lock()
+								store = make(map[string]string)
+								mu.Unlock()
+							}()
+							conn.Write([]byte("+OK\r\n"))
+						
+						default:
+							conn.Write([]byte("-ERR invalid option for 'FLUSHALL' command\r\n"))
+					}
+
+				case "EXIT":
+					conn.Write([]byte("+OK\r\n"))
+					return //close connection
 					
 
 				
