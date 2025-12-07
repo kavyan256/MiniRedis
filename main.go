de47a97bea4ea8981e4b9a89c2218933f4653d41
kavyan2 <IIT2024245@iiita.ac.in>
2025-12-08 01:34:42 +0530

    modified error replies to work with redis-cli

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var mu sync.RWMutex				//mutex for concurrent access
 func main() {
 	listener, err := net.Listen("tcp", ":6379") //listen on port 8080
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("-Error starting server:", err)
 		return
 	}
 
@@ -41,12 +41,12 @@ func parseResp(reader *bufio.Reader) ([]string, error) {
 
 	line = strings.TrimSpace(line)
 	if len(line) == 0 || line[0] != '*' {
-		return nil, fmt.Errorf("invalid RESP format.(* expected)")
+		return nil, fmt.Errorf("-ERR invalid RESP format.(* expected)")
 	}
 
 	numArgs, err := strconv.Atoi(line[1:])  // ASCII to int
 	if( err != nil || numArgs <= 0 ){
-		return nil, fmt.Errorf("invalid RESP length")
+		return nil, fmt.Errorf("-ERR invalid RESP length")
 	}
 
 	args := make([]string, numArgs)
@@ -54,32 +54,32 @@ func parseResp(reader *bufio.Reader) ([]string, error) {
 	for i := 0 ; i<numArgs ; i++ {
 		lenline, err := reader.ReadString('\n')
 		if err != nil {
-			return nil, fmt.Errorf("io error reading bulk header")
+			return nil, fmt.Errorf("-ERR io error reading bulk header")
 		}
 
 		lenline = strings.TrimSpace(lenline)
 		if len(lenline) == 0 || lenline[0] != '$' {
-			return nil, fmt.Errorf("invalid RESP format. $ expected")
+			return nil, fmt.Errorf("-ERR invalid RESP format. $ expected")
 		}
 
 		length, err := strconv.Atoi(lenline[1:])
 		if err != nil {
-			return nil, fmt.Errorf("invalid bulk length")
+			return nil, fmt.Errorf("-ERR invalid bulk length")
 		}
 
 		buff := make([]byte, length)
 		if _ , err := io.ReadFull(reader, buff); err != nil {
-			return nil, fmt.Errorf("io error reading bulk data")
+			return nil, fmt.Errorf("-ERR io error reading bulk data")
 		}
 
 		crlf, err := reader.ReadString('\n')
 		if err != nil {
-			return nil, fmt.Errorf("protocol error: missing terminating CRLF")
+			return nil, fmt.Errorf("-ERR protocol error: missing terminating CRLF")
 		}
 		if !strings.HasSuffix(crlf, "\r\n") {
-			return nil, fmt.Errorf("protocol error: invalid terminating CRLF")
+			return nil, fmt.Errorf("-ERR protocol error: invalid terminating CRLF")
 		}
-		
+
 		args[i] = string(buff)
 	}
 
@@ -96,7 +96,7 @@ func handleConnection(conn net.Conn) {
 	for {
 		args, err := parseResp(reader)
 		if err != nil {
-			conn.Write([]byte("ERR protocol error: " + err.Error() + "\r\n"))
+			conn.Write([]byte("-ERR protocol error: " + err.Error() + "\r\n"))
 			return
 		}
 
@@ -108,7 +108,7 @@ func handleConnection(conn net.Conn) {
 		switch command {
 			case "SET":
 				if len(args) != 3 {
-					conn.Write([]byte("ERR wrong number of arguments for 'SET' command\r\n"))
+					conn.Write([]byte("-ERR wrong number of arguments for 'SET' command\r\n"))
 					continue
 				}
 				//example "SET A 10"
@@ -119,11 +119,11 @@ func handleConnection(conn net.Conn) {
 				store[key] = value
 				mu.Unlock()
 				
-				conn.Write([]byte("OK\r\n"))
+				conn.Write([]byte("+OK\r\n"))
 
 				case "GET":
 					if len(args) != 2 {
-						conn.Write([]byte("ERR wrong number of arguments for 'GET' command\r\n"))
+						conn.Write([]byte("-ERR wrong number of arguments for 'GET' command\r\n"))
 						continue
 					}
 
@@ -134,14 +134,14 @@ func handleConnection(conn net.Conn) {
 					mu.RUnlock()
 					
 					if exists {
-						conn.Write([]byte(value + "\r\n"))
+						conn.Write([]byte("$" + strconv.Itoa(len(value)) + "\r\n" + value + "\r\n"))
 					} else {
-						conn.Write([]byte("nil\r\n"))
+						conn.Write([]byte("$-1\r\n"))
 					}
 				
 				case "DEL":
 					if len(args) != 2 {
-						conn.Write([]byte("ERR wrong number of arguments for 'DEL' command\r\n"))
+						conn.Write([]byte("-ERR wrong number of arguments for 'DEL' command\r\n"))
 						continue
 					}
 					
@@ -153,13 +153,13 @@ func handleConnection(conn net.Conn) {
 					if exists {
 						delete(store, key)
 						mu.Unlock()
-						conn.Write([]byte("1(delete success)\r\n")) //1 indicates one key deleted
+						conn.Write([]byte(":1\r\n")) //1 indicates one key deleted
 					} else {
 						mu.Unlock()
-						conn.Write([]byte("0(delete failed)\r\n")) //0 indicates no key deleted
+						conn.Write([]byte(":0\r\n")) //0 indicates no key deleted
 					}
 					default:
-						conn.Write([]byte("ERR unknown command\r\n"))
+						conn.Write([]byte("-ERR something\r\n"))
 		}
 	
 	}
