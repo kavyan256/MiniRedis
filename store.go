3c1c08fe22ce3e9cf0828519f2bbd7204dade7bc
kavyan2 <IIT2024245@iiita.ac.in>
2025-12-14 16:51:15 +0530

    removed unused old elements

--- store.go
+++ store.go
@@ -8,17 +8,11 @@ const NumDatabases = 16
 
 var databases [NumDatabases]map[string]Entry
 
-var db map[string]Entry
-
 func init() {
 	for i := 0; i < NumDatabases; i++ {
 		databases[i] = make(map[string]Entry)
 	}
-	db = databases[0]
 }
-
-var usedMemory int64 = 0
-var lastAccess = map[string]int64{}
 
 var mu sync.RWMutex
 var aofMu sync.Mutex
